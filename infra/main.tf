9d21cad9c4d119c9d1f4b87c407787b06be141a8
Santiago Vasquez <svasquez@pivotal.io>
2021-02-24 21:36:01 -0500

    Add first resources to infra

--- infra/main.tf
+++ infra/main.tf
@@ -7,8 +7,14 @@ provider "azurerm" {
   features {}
 }
 
-resource "random_string" "random" {
-  length = 6
-  special = false
-  upper = false
+resource "azurerm_resource_group" "pact-broker-rg" {
+  name     = "pact-broker-rg"
+  location = "eastus2"
+}
+
+resource "azurerm_virtual_network" "pact-broker-vn" {
+  name                = "pact-broker-vn"
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastus2"
+  resource_group_name = azurerm_resource_group.pact-broker-rg.name
 }
