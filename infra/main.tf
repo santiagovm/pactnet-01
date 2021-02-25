58482ab0ba200a8b8e5ff76b3980b17e4b81084b
Santiago Vasquez <santiago@vasquezhouse.com>
2021-02-25 07:37:03 -0500

    Pact Broker resource group created out of band
    Co-authored-by: John Wick <jwick@badass.org>

--- infra/main.tf
+++ infra/main.tf
@@ -7,14 +7,9 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "pact-broker-rg" {
-  name     = "pact-broker-rg"
-  location = "eastus2"
-}
-
 resource "azurerm_virtual_network" "pact-broker-vn" {
   name                = "pact-broker-vn"
   address_space       = ["10.0.0.0/16"]
   location            = "eastus2"
-  resource_group_name = azurerm_resource_group.pact-broker-rg.name
+  resource_group_name = "pact-broker-rg"
 }
