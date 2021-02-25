ef6164bb938a15728b7f908550c32222239f1e69
Santiago Vasquez <santiago@vasquezhouse.com>
2021-02-25 08:52:40 -0500

    Add key vault
    Co-authored-by: John Wick <jwick@badass.org>

--- infra/main.tf
+++ infra/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_resource_group" "pact-broker-rg" {
 resource "azurerm_virtual_network" "pact-broker-vn" {
   name                = "pact-broker-vn"
   address_space       = ["10.0.0.0/16"]
-  location            = "eastus2"
+  location            = azurerm_resource_group.pact-broker-rg.location
   resource_group_name = azurerm_resource_group.pact-broker-rg.name
 }
 
@@ -26,4 +26,35 @@ resource "azurerm_subnet" "postgres-sn" {
   address_prefix       = "10.0.1.0/24"
 
   enforce_private_link_endpoint_network_policies = true
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "the-key-vault" {
+  name                        = "the-key-vault"
+  location                    = azurerm_resource_group.pact-broker-rg.location
+  resource_group_name         = azurerm_resource_group.pact-broker-rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "get",
+    ]
+
+    secret_permissions = [
+      "get", "backup", "delete", "list", "purge", "recover", "restore", "set",
+    ]
+
+    storage_permissions = [
+      "get",
+    ]
+  } 
 }
