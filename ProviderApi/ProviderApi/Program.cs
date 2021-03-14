4535d9175939bb4c4e1dbf8e7c81eb44a80e0ed7
Santiago Vasquez <svasquez@pivotal.io>
2021-03-14 13:34:52 -0400

    Add some non-contract related change to provider

--- ProviderApi/ProviderApi/Program.cs
+++ ProviderApi/ProviderApi/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -8,6 +9,7 @@ namespace PactNet01.ProviderApi
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("some change not related to contracts 01");
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
